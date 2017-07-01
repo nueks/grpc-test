956bcc6906342664f1693a1ebf928ca305d32370
Lee Sangkeun <nueks.lee@navercorp.com>
2017-07-01 12:40:53 +0900

    코드정리

--- echo_server_master.cc
+++ echo_server_master.cc
@@ -28,106 +28,34 @@ using echo::EchoRequest;
 using echo::EchoResponse;
 using echo::Echo;
 
-/*
-class ThreadPool
+
+class StubPool
 {
 private:
-	struct JobInfo
-	{
-		std::function<void(bool)>* fn;
-	};
-
-	bool exit_{false};
-	std::deque< std::function<void(bool)>* > queue_;
-	std::condition_variable cond_;
-	mutable std::mutex mutex_;
-	std::vector<std::thread> threads_;
-
-	ThreadPool(const ThreadPool&) = delete;
-	ThreadPool& operator=(const ThreadPool&) = delete;
-	ThreadPool() = default;
+	std::shared_ptr<Channel> channel_{grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials())};
+	std::unique_ptr<Echo::Stub> stub_{ Echo::NewStub(channel_) };
 
 public:
-	static ThreadPool& instance()
+	static StubPool& instance()
 	{
-		static ThreadPool instance;
-		return instance;
+		static StubPool inst;
+		return inst;
 	}
 
-	~ThreadPool() noexcept
+	Echo::Stub* stub()
 	{
-		std::cout << "dtor ThreadPool" << std::endl;
-		threads_.clear();
-		queue_.clear();
-	}
-
-	void init(size_t num_thread)
-	{
-		for (auto i = 0; i < num_thread; ++i)
-		{
-			threads_.emplace_back(&ThreadPool::main, this);
-		}
-	}
-
-	void stop()
-	{
-		{
-			std::lock_guard<std::mutex> lock(mutex_);
-			exit_ = true;
-		}
-		cond_.notify_all();
-		std::for_each(begin(threads_), end(threads_), [](std::thread& t){ t.join(); });
-
-		for (auto job : queue_)
-		{
-			std::cout << "job finalize" << std::endl;
-			(*job)(false);
-		}
-	}
-
-	void push(std::function<void(bool)>* h)
-	{
-		if (exit_)
-		{
-			std::cout << "push to finalized worker" << std::endl;
-			(*h)(false);
-			return;
-		}
-
-		{
-			std::lock_guard<std::mutex> lock(mutex_);
-			queue_.emplace_back(h);
-		}
-		cond_.notify_one();
+		return stub_.get();
 	}
 
 private:
-	void main()
+	StubPool()
 	{
-		std::cout << "worker started" << std::endl;
-		while (true)
-		{
-			//JobInfo info;
-			std::function<void(bool)>* job;
-			{
-				std::unique_lock<std::mutex> lock(mutex_);
-				cond_.wait(lock, [this]{ return exit_ || !queue_.empty(); });
-
-				if (exit_)
-					break;
-
-				job = std::move(queue_.front());
-				queue_.pop_front();
-			}
-			(*job)(true);
-		}
-		std::cout << "worker end" << std::endl;
 	}
 };
-*/
+
 
 template <typename ServiceType, typename RequestType, typename ResponseType, typename ImplType>
-struct ServerRpc// : public std::enable_shared_from_this<ServerRpc<ServiceType, RequestType, ResponseType, ImplType> >
+struct ServerRpc
 {
 protected:
 	ServiceType* service_;
@@ -141,8 +69,6 @@ protected:
 	std::function<void(bool)> on_read_;
 	std::function<void(bool)> on_finish_;
 
-	std::function<void(bool)> job_;
-
 	using InitHandler=std::function<void(ServiceType*,
 										 ServerContext*,
 										 RequestType*,
@@ -155,21 +81,8 @@ public:
 	ServerRpc(ServiceType* service, ServerCompletionQueue* cq)
 		: service_(service), cq_(cq), responder_(&ctx_)
 	{
-		//on_read_ = [this](bool ok) mutable { this->onRead(ok); };
-		//on_finish_ = [this](bool ok) mutable { this->onFinish(ok); };
-
-
-		// job_ = [this](bool ok){
-		// 	if (!ok)
-		// 	{
-		// 		delete static_cast<ImplType*>(this);
-		// 	}
-		// 	else
-		// 	{
-		// 		static_cast<ImplType&>(*this).process();
-		// 		responder_.Finish(response_, Status::OK, &on_finish_);
-		// 	}
-		// };
+		on_read_ = [this](bool ok) mutable { this->onRead(ok); };
+		on_finish_ = [this](bool ok) mutable { this->onFinish(ok); };
 	}
 
 	~ServerRpc()
@@ -178,12 +91,6 @@ public:
 	}
 	void init(const InitHandler& handler)
 	{
-		on_read_ = [this](bool ok) mutable { this->onRead(ok); };
-		on_finish_ = [this](bool ok) mutable { this->onFinish(ok); };
-
-		//on_read_ = [self = this->shared_from_this()](bool ok) mutable { self->onRead(ok); };
-		//on_finish_ = [self = this->shared_from_this()](bool ok) mutable { self->onFinish(ok); };
-
 		handler(service_, &ctx_, &request_, &responder_, cq_, cq_, &on_read_);
 	}
 
@@ -192,65 +99,32 @@ private:
 	{
 		if (!ok)
 		{
-			//std::cout << "---------------" << std::endl;
-			//delete static_cast<ImplType*>(this);
+			delete static_cast<ImplType*>(this);
 			return;
 		}
 
 		static_cast<ImplType&>(*this).clone();
-		//ThreadPool::instance().push(&job_);
 		static_cast<ImplType&>(*this).process();
 	}
 
 	void onFinish(bool ok)
 	{
-		//std::cout << this->shared_from_this().use_count() << std::endl;
-		//if (!ok)
-		//	std::cout << "===================" << std::endl;
 		delete static_cast<ImplType*>(this);
 	}
-};
-
-
-
-class Pool
-{
-public:
-	static Pool& instance()
-	{
-		static Pool inst;
-		return inst;
-	}
-
-private:
-	Pool()
-	{
-		//channel_ = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
-		//stub_ = Echo::NewStub(channel_);
-	}
-
-public:
-	Echo::Stub* stub()
-	{
-		return stub_.get();
-	}
-
-private:
-	std::shared_ptr<Channel> channel_{grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials())};
-	std::unique_ptr<Echo::Stub> stub_{ Echo::NewStub(channel_) };
 };
 
 
 struct EchoRpc : public ServerRpc<Echo::AsyncService, EchoRequest, EchoResponse, EchoRpc>
 {
 private:
+	constexpr static int expected_{4}; 	// 하단 서버로 보낼 요청 갯수.
+
 	std::string tag_;
 	CompletionQueue* ccq_;
 	std::mutex mutex_;
 
 	int num_{0};
 
-	//ClientContext context_;
 	struct ClientCall
 	{
 		ClientContext context;
@@ -269,8 +143,6 @@ public:
 		  ccq_(cq)
 	{
 		init(&Echo::AsyncService::RequestProcess);
-		//context_.set_compression_algorithm(GRPC_COMPRESS_GZIP);
-		//fn_ = [this](bool ok){ callback(ok); };
 	}
 
 	~EchoRpc()
@@ -280,22 +152,15 @@ public:
 
 	void process()
 	{
-		//gpr_log(GPR_DEBUG, "process");
-
 		using namespace std::chrono;
-
-		system_clock::time_point deadline = system_clock::now() + milliseconds(1000);
-		//context_.set_deadline(deadline);
+		auto deadline = system_clock::now() + milliseconds(1000);
 
 		std::lock_guard<std::mutex> lock(mutex_);
-		auto stub = Pool::instance().stub();
-		for (auto i = 0; i < 2; ++i)
+		auto stub = StubPool::instance().stub();
+		for (auto i = 0; i < expected_; ++i)
 		{
-			//gpr_log(GPR_DEBUG, "send to slave: %d", i);
-
 			auto call = std::make_unique<ClientCall>();
 
-			//ClientCall call;
 			call->context.set_compression_algorithm(GRPC_COMPRESS_GZIP);
 			call->context.set_deadline(deadline);
 
@@ -307,51 +172,35 @@ public:
 	}
 
 	EchoRpc* clone()
-	//std::shared_ptr<EchoRpc> clone()
 	{
-		//return new EchoRpc(service_, cq_, ccq_, tag_);
-		//return std::make_shared<EchoRpc>(service_, cq_, ccq_, tag_);
-		//init(&Echo::AsyncService::RequestProcess);
-		//auto rpc = std::make_shared<EchoRpc>(service_, cq_, ccq_, tag_);
 		auto rpc = new EchoRpc(service_, cq_, ccq_, tag_);
-		//rpc->init(&Echo::AsyncService::RequestProcess);
 		return rpc;
-
 	}
 
 	void callback(bool ok, int i)
 	{
-		// 끊기거나, timeout 이 발생해도 이벤트가 발생해야 한다.
-		// 등록한 이벤트를 모두 받은 다음에만 삭제 할 수 있다.
-
-		// 한 쓰레드가 callback 처리중인데, 다른 쓰레드가 먼저 callback 을 처리하고 delete 까지 한다면?
 		{
-		std::lock_guard<std::mutex> lock(mutex_);
-		if (!ok)
-		{
-			// .......
+			std::lock_guard<std::mutex> lock(mutex_);
+			if (!ok)
+			{
+				gpr_log(GPR_ERROR, "TODO: check this case");
+				//delete this;
+			}
 
-			gpr_log(GPR_ERROR, "TODO: check this case");
-			//delete this;
+			//gpr_log(GPR_INFO, "status: %d", calls_[i]->status.ok());
 
+			num_++;
+			//gpr_log(GPR_INFO, "callback with index:%d, num:%d", i, num_);
+			if (num_ != expected_)
+				return;
 		}
 
-		//gpr_log(GPR_INFO, "status: %d", calls_[i]->status.ok());
-
-
-
-		num_++;
-		//gpr_log(GPR_INFO, "callback with index:%d, num:%d", i, num_);
-		if (num_ != 2)
-			return;
-		}
-		if (num_ == 2)
-		{
-			//usleep(10000);
-			//gpr_log(GPR_DEBUG, "response to client");
-			response_.set_message(request_.message());
-			responder_.Finish(response_, Status::OK, &on_finish_);
-		}
+		// IMPORTANT!! - mutex를 unlock하고 Finish를 호출해야 한다.
+		// Finish를 호출하면 on_finish_에서 EchoRpc 객체를 delete 한다.
+		// 이 함수(callback)가 종료하기 전에 위 작업이 먼저 수행될 수 있다.
+		// 그러면 삭제된 mutex를 unlock 하면서 core가 발생한다.
+		response_.set_message(request_.message());
+		responder_.Finish(response_, Status::OK, &on_finish_);
 	}
 };
 
@@ -360,24 +209,23 @@ class ServerImpl final
 {
 private:
 	Echo::AsyncService service_;
-	std::unique_ptr<CompletionQueue> ccq_{new CompletionQueue};
-	std::unique_ptr<ServerCompletionQueue> cq_;
 	std::unique_ptr<Server> server_;
+
+	std::unique_ptr<ServerCompletionQueue> cq_;
+	std::unique_ptr<CompletionQueue> ccq_{new CompletionQueue};
 
 	std::vector<std::thread> threads_;
 
 public:
 	~ServerImpl()
 	{
-		//std::cout << "dtor of ServerImpl" << std::endl;
-		gpr_log(GPR_INFO, "dtor of ServerImpl");
-
+		gpr_log(GPR_INFO, "shutdown Server");
 		server_->Shutdown();
 
-		gpr_log(GPR_INFO, "shutdown cq");
+		gpr_log(GPR_INFO, "shutdown ServerCompletionQueue");
 		cq_->Shutdown();
 
-		gpr_log(GPR_INFO, "shutdown ccq");
+		gpr_log(GPR_INFO, "shutdown Client CompletionQueue");
 		ccq_->Shutdown();
 
 		std::for_each(begin(threads_), end(threads_), [](std::thread& t){ t.join(); });
@@ -399,12 +247,19 @@ public:
 
 		init();
 
+		// 클라이언트의 연결을 담당하는 thread (server thread)는 네트웍 I/O만을 담당한다.
+		// 이경우 thread를 늘리면 오히려 성능이 저하된다.
+		// 성능을 끌어내기 위해서는 ServerCompletionQueue를 thread 당 하나씩 할당해야 한다.
 		for (auto i = 0; i < 1; ++i)
 		{
 			threads_.emplace_back(&ServerImpl::HandleRpcs, this);
 		}
 
-		for (auto i = 0; i< 8; ++i)
+		// 하단 서버(slave) 와의 연결을 담당하는 thread는 sort/merge 등을 처리해야 한다.
+		// I/O 작업만 수행하는 ServerCompletionQueue와 달리 thread 갯수를 늘릴 수 있다.
+		// 단, sort/merge 작업의 부하가 작으면 thread 갯수가 작을 수록 유리하다.
+		// 성능을 끌어내기 위해서는 CompletionQueue를 thread 당 하나씩 할당해야 한다.
+		for (auto i = 0; i< 2; ++i)
 		{
 			threads_.emplace_back(&ServerImpl::HandleClients, this);
 		}
@@ -412,9 +267,7 @@ public:
 
 	void init()
 	{
-		auto rpc = new EchoRpc(&service_, cq_.get(), ccq_.get(), "test");
-		//auto rpc = std::make_shared<EchoRpc>(&service_, cq_.get(), ccq_.get(), "test");
-		//rpc->init(&Echo::AsyncService::RequestProcess);
+		new EchoRpc(&service_, cq_.get(), ccq_.get(), "test");
 	}
 
 	void HandleRpcs()
@@ -432,13 +285,11 @@ public:
 			}
 			else if (ret == CompletionQueue::NextStatus::SHUTDOWN)
 			{
-				//std::cout << "cq shutdown" << std::endl;
-				//gpr_log(GPR_INFO, "cq shutdowned");
+				gpr_log(GPR_INFO, "ServerCompletionQueue shutdowned");
 				break;
 			}
 		}
 
-		//std::cout << "end of HandleRpcs" << std::endl;
 		gpr_log(GPR_INFO, "end of HandleRpcs");
 	}
 
@@ -450,7 +301,6 @@ public:
 		while (true)
 		{
 			auto ret = ccq_->Next((void**)&tag, &ok);
-			//gpr_log(GPR_DEBUG, "got client event. ret = %d", ret);
 			if (ret == CompletionQueue::NextStatus::GOT_EVENT)
 			{
 				//gpr_log(GPR_DEBUG, "got client event with ok=%d", ok);
@@ -459,39 +309,42 @@ public:
 			}
 			else if (ret == CompletionQueue::NextStatus::SHUTDOWN)
 			{
-				//std::cout << "ccq shutdown" << std::endl;
-				//gpr_log(GPR_INFO, "ccq shutdowned");
+				gpr_log(GPR_INFO, "Client CompletionQueue shutdowned");
 				break;
 			}
 		}
 		gpr_log(GPR_INFO, "end of HandleClients");
-		//std::cout << "end of HandleClients" << std::endl;
 	}
 };
+
+
+void wait(std::initializer_list<int> sigs)
+{
+	sigset_t set;
+	int signum;
+	sigemptyset(&set);
+	for (auto sig : sigs)
+	{
+		sigaddset(&set, sig);
+	}
+	pthread_sigmask(SIG_BLOCK, &set, NULL);
+	sigwait(&set, &signum);
+}
 
 
 int main(int argc, char** argv)
 {
 	gpr_set_log_verbosity(GPR_LOG_SEVERITY_DEBUG);
-	Pool::instance().stub();
-	//ThreadPool::instance().init(8);
+
 	ServerImpl server;
 	server.run();
 
-	//std::cout << "set signal" << std::endl;
-	gpr_log(GPR_INFO, "wait signal");
-	sigset_t set;
-	int signum;
-	sigemptyset(&set);
-	sigaddset(&set, SIGQUIT);
-	sigaddset(&set, SIGTERM);
-	sigaddset(&set, SIGINT);
-	pthread_sigmask(SIG_BLOCK, &set, NULL);
-	sigwait(&set, &signum);
+	// connect to slave
+	StubPool::instance().stub();
 
-	gpr_log(GPR_INFO, "stop server");
-	//std::cout << "stop server" << std::endl;
-	//ThreadPool::instance().stop();
+	gpr_log(GPR_INFO, "Server started");
+	wait({SIGQUIT, SIGTERM, SIGINT});
+	gpr_log(GPR_INFO, "stop Server");
 
 	return 0;
 }
